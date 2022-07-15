de05ec233ba34e6b5052be3f8dcb809ad51ab183
fbslo <admin@fbslo.net>
2022-07-15 12:58:31 +0200

    modify minter to use timestamps instead of block numbers

--- contracts/Minter.sol
+++ contracts/Minter.sol
@@ -14,16 +14,16 @@ contract SPSMinter {
   address public admin;
   /// @notice Address of the token to mint
   IMintable public immutable token;
-  /// @notice Block number when mint() was last called
-  uint256 public lastMintBlock;
+  /// @notice Timestamp of the block when mint() was last called
+  uint256 public lastMintTimestamp;
   /// @notice Total number of tokens already minted
   uint256 public totalMinted;
   /// @notice Maximum number of tokens minted, 3B (with 18 decimal places)
   uint256 constant public cap = 3000000000 ether;
   /// @notice Maximum number of pools
   uint256 constant public poolsCap = 100;
-  /// @notice Maximum amount per block to each pool
-  uint256 constant public maxToPoolPerBlock = 50 ether;
+  /// @notice Maximum amount per second to each pool
+  uint256 constant public maxToPoolPerSecond = 50 ether;
   // @notice minimum payout before it's rounded to 0
   uint256 constant public minimumPayout = 0.1 ether;
   /// @notice basis points
@@ -32,8 +32,8 @@ contract SPSMinter {
   /// @notice Struct to store information about each pool
   struct Pool {
     address receiver;
-    uint256 amountPerBlock;
-    uint256 reductionBlocks;
+    uint256 amountPerSecond;
+    uint256 reductionSeconds;
     uint256 reductionBps;
     uint256 lastUpdate;
     address callAddress;
@@ -44,9 +44,9 @@ contract SPSMinter {
   /// @notice Emitted when mint() is called
   event Mint(address indexed receiver, uint256 amount);
   /// @notice Emitted when pool is added
-  event PoolAdded(address indexed newReceiver, uint256 newAmount, uint256 newReductionBlocks, uint256 newReductionBps, uint256 newLastUpdate, address newCallAddress);
+  event PoolAdded(address indexed newReceiver, uint256 newAmount, uint256 newReductionSeconds, uint256 newReductionBps, uint256 newLastUpdate, address newCallAddress);
   /// @notice Emitted when pool is updated
-  event PoolUpdated(uint256 index, address indexed newReceiver, uint256 newAmount, uint256 newReductionBlocks, uint256 newReductionBps, uint256 newLastUpdate, address newCallAddress);
+  event PoolUpdated(uint256 index, address indexed newReceiver, uint256 newAmount, uint256 newReductionSeconds, uint256 newReductionBps, uint256 newLastUpdate, address newCallAddress);
   /// @notice Emitted when pool is removed
   event PoolRemoved(uint256 index, address indexed receiver, uint256 amount);
   /// @notice Emitted when admin address is updated
@@ -64,13 +64,13 @@ contract SPSMinter {
    * @param startBlock Initial lastMint block
    * @param newAdmin Initial admin address
    */
-  constructor(address newToken, uint256 startBlock, address newAdmin){
-    require(startBlock >= block.number, "SPSMinter: Start block must be above current block");
+  constructor(address newToken, uint256 startTimestamp, address newAdmin){
+    require(startTimestamp >= block.timestamp, "SPSMinter: Start timestamp must be above current timestamp");
     require(newToken != address(0), 'SPSMinter: Token cannot be address 0');
     require(newAdmin != address(0), 'SPSMinter: Admin cannot be address 0');
 
     token = IMintable(newToken);
-    lastMintBlock = startBlock;
+    lastMintTimestamp = startTimestamp;
     admin = newAdmin;
 
     require(token.decimals() == 18, "SPSMinter: Token doesn't have 18 decimals");
@@ -83,19 +83,19 @@ contract SPSMinter {
    */
   function mint() public {
     require(totalMinted < cap, "SPSMinter: Cap reached");
-    require(block.number > lastMintBlock, "SPSMinter: Mint block not yet reached");
+    require(block.timestamp > lastMintTimestamp, "SPSMinter: Mint timestamp not yet reached");
     updateAllEmissions();
 
     uint256 mintDifference;
     unchecked {
-      mintDifference = block.number - lastMintBlock;
+      mintDifference = block.timestamp - lastMintTimestamp;
     }
 
-    lastMintBlock = block.number;
+    lastMintTimestamp = block.timestamp;
 
     uint256 poolsLength = pools.length;
     for (uint256 i = 0; i < poolsLength;){
-      uint256 amount = pools[i].amountPerBlock * mintDifference;
+      uint256 amount = pools[i].amountPerSecond * mintDifference;
 
       if(totalMinted + amount >= cap){
         unchecked {
@@ -120,15 +120,15 @@ contract SPSMinter {
    * @notice Add new pool, can be called by admin
    * @param newReceiver Address of the receiver
    * @param newAmount Amount of tokens per block
-   * @param newReductionBlocks Number of blocks between emission reduction
+   * @param newReductionSeconds Number of seconds between emission reduction
    * @param newReductionBps Number of basis points to reduce emission
    */
-  function addPool(address newReceiver, uint256 newAmount, uint256 newReductionBlocks, uint256 newReductionBps, address newCallAddress) external onlyAdmin {
+  function addPool(address newReceiver, uint256 newAmount, uint256 newReductionSeconds, uint256 newReductionBps, address newCallAddress) external onlyAdmin {
     require(pools.length < poolsCap, 'SPSMinter: Pools cap reached');
-    require(newAmount <= maxToPoolPerBlock, 'SPSMinter: Maximum amount per block reached');
+    require(newAmount <= maxToPoolPerSecond, 'SPSMinter: Maximum amount per second reached');
     require(newReductionBps <= BPS, "SPSMinter: newReductionBps cannot be larger than max allowed");
-    pools.push(Pool(newReceiver, newAmount, newReductionBlocks, newReductionBps, block.number, newCallAddress));
-    emit PoolAdded(newReceiver, newAmount, newReductionBlocks, newReductionBps, block.number, newCallAddress);
+    pools.push(Pool(newReceiver, newAmount, newReductionSeconds, newReductionBps, block.timestamp, newCallAddress));
+    emit PoolAdded(newReceiver, newAmount, newReductionSeconds, newReductionBps, block.timestamp, newCallAddress);
   }
 
   /**
@@ -136,15 +136,15 @@ contract SPSMinter {
    * @param index Index in the array of the pool
    * @param newReceiver Address of the receiver
    * @param newAmount Amount of tokens per block
-   * @param newReductionBlocks Number of blocks between emission reduction
+   * @param newReductionSeconds Number of seconds between emission reduction
    * @param newReductionBps Number of basis points (1 bps = 1/100th of 1%) to reduce emission
    */
-  function updatePool(uint256 index, address newReceiver, uint256 newAmount, uint256 newReductionBlocks, uint256 newReductionBps, address newCallAddress) external onlyAdmin {
+  function updatePool(uint256 index, address newReceiver, uint256 newAmount, uint256 newReductionSeconds, uint256 newReductionBps, address newCallAddress) external onlyAdmin {
     require(newAmount <= maxToPoolPerBlock, 'SPSMinter: Maximum amount per block reached');
     require(newReductionBps <= BPS, "SPSMinter: newReductionBps cannot be larger than max allowed");
     mint();
-    pools[index] = Pool(newReceiver, newAmount, newReductionBlocks, newReductionBps, block.number, newCallAddress);
-    emit PoolUpdated(index, newReceiver, newAmount, newReductionBlocks, newReductionBps, block.number, newCallAddress);
+    pools[index] = Pool(newReceiver, newAmount, newReductionSeconds, newReductionBps, block.timestamp, newCallAddress);
+    emit PoolUpdated(index, newReceiver, newAmount, newReductionSeconds, newReductionBps, block.timestamp, newCallAddress);
   }
 
   /**
@@ -152,10 +152,10 @@ contract SPSMinter {
    * @param index Index in the array of the pool
    */
   function updateEmissions(uint256 index) public {
-    if (block.number - pools[index].lastUpdate > pools[index].reductionBlocks){
-      pools[index].amountPerBlock = (pools[index].amountPerBlock * (BPS - pools[index].reductionBps)) / BPS;
-      if (minimumPayout > pools[index].amountPerBlock) pools[index].amountPerBlock = 0;
-      pools[index].lastUpdate = block.number;
+    if (block.timestamp - pools[index].lastUpdate > pools[index].reductionSeconds){
+      pools[index].amountPerSecond = (pools[index].amountPerSecond * (BPS - pools[index].reductionBps)) / BPS;
+      if (minimumPayout > pools[index].amountPerSecond) pools[index].amountPerSecond = 0;
+      pools[index].lastUpdate = block.timestamp;
 
       if (pools[index].callAddress != address(0)){
         // Call external contract, won't revert on failure. Used to "notify" other contract that there was a change
@@ -183,7 +183,7 @@ contract SPSMinter {
     require(pools.length > index, 'Index is not valid');
 
     mint();
-    emit PoolRemoved(index, pools[index].receiver, pools[index].amountPerBlock);
+    emit PoolRemoved(index, pools[index].receiver, pools[index].amountPerSecond);
 
     unchecked {
       pools[index] = pools[pools.length - 1];
